8981c9d82ab461332b1149e0e3ac7634189fea82
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-05-22 20:20:29 -0400

    C# receiver commented out

--- UDP_Connection/Program.cs
+++ UDP_Connection/Program.cs
@@ -12,7 +12,7 @@ namespace UDP_Connection
         static string ip_address = "127.0.0.1";
         static int port = 8008;
 
-        static UDP_Receiver receiver = new UDP_Receiver(port);
+       // static UDP_Receiver receiver = new UDP_Receiver(port);
         static UDP_Sender sender = new UDP_Sender(ip_address, port);
 
         static public bool isRunning = true;
@@ -20,36 +20,38 @@ namespace UDP_Connection
         /// <summary>
         /// Starts the receiver thread.
         /// </summary>
-        public static void startReceiverThread()
-        {
-            Thread newThread = new Thread(ReceiverThread);
-			newThread.Start();
-        }
+   //     public static void startReceiverThread()
+   //     {
+   //         Thread newThread = new Thread(ReceiverThread);
+			//newThread.Start();
+        //}
 
         /// <summary>
         /// Takes out message if any on the queue.
         /// </summary>
-		public static void ReceiverThread()
-		{
-			string msg;
-			BlockingCollection<string> msgs;
+       
 
-			msgs = receiver.getMsgQueue();
+		//public static void ReceiverThread()
+		//{
+		//	string msg;
+		//	BlockingCollection<string> msgs;
 
-			while (isRunning)
-			{
-				if (msgs.TryTake(out msg, 1000))
-				{
-					Console.WriteLine("Message Received: {0}", msg);
-				}
-			}
-		}
+		//	msgs = receiver.getMsgQueue();
+
+		//	while (isRunning)
+		//	{
+		//		if (msgs.TryTake(out msg, 1000))
+		//		{
+		//			Console.WriteLine("Message Received: {0}", msg);
+		//		}
+		//	}
+		//}
 
 
         static void Main(string[] args)
         {
-            receiver.start();
-            startReceiverThread();
+            //receiver.start();
+            //startReceiverThread();
 
             while (true)
             {
@@ -61,9 +63,9 @@ namespace UDP_Connection
             }
             Console.WriteLine("Stopping Client.");
 
-            receiver.stop();
+//            receiver.stop();
             isRunning = false;
-            receiver.getMsgQueue().Add(null);
+  //          receiver.getMsgQueue().Add(null);
 
             Console.WriteLine("Everything is done!");
 
