c1903968a1365c2b49008227eff550528ab50a4b
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-05-23 01:30:16 -0400

    Read from file in common-line arguments

--- Java/src/Receiver.java
+++ Java/src/Receiver.java
@@ -1,67 +1,93 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Zhiling on 5/20/17.
  */
-public class Receiver implements Runnable{
-	
+public class Receiver implements Runnable {
+
     // unbounded
-    BlockingQueue<byte[]> receivedMsgs = new LinkedBlockingQueue<byte[]>();
-    
+    BlockingQueue<byte[]> receivedMsgs = new ArrayBlockingQueue<byte[]>(100);
+
     private DatagramSocket mySocket;
     private int myPort;
 
     private boolean isRunning = true;
-    
+
     public int timeOut = 5000;
-    
-    public Receiver(int port)   {
+
+    public Receiver(int port) {
         myPort = port;
         try {
             mySocket = new DatagramSocket(myPort);
-            mySocket.setSoTimeout(timeOut);		// wait for 0.5 second for data
+            mySocket.setSoTimeout(timeOut);        // wait for 0.5 second for data
         } catch (SocketException e) {
             e.printStackTrace();
         }
     }
 
-	@Override
-	public void run() {
-		DatagramPacket packet = null;
-        while (isRunning)   {
+    @Override
+    public void run() {
+        DatagramPacket packet;
+        while (isRunning) {
             // buffer is filled with the data received
             try {
-            	InetAddress address = InetAddress.getByName ("127.0.0.1");
-            	//InetAddress address = InetAddress.getLocalHost();
+                InetAddress address = InetAddress.getByName("127.0.0.1");
                 byte[] msg = new byte[1024];
-            	packet = new DatagramPacket(msg, msg.length, address, myPort );
-			    mySocket.receive(packet);	// blocks until it receives a datagram
-			    receivedMsgs.offer(msg, timeOut, TimeUnit.MILLISECONDS);
-			    System.out.println(new String(msg));
+//                packet = new DatagramPacket(msg, msg.length, address, myPort);
+                packet = new DatagramPacket(msg, msg.length);
+                mySocket.receive(packet);    // blocks until it receives a datagram
+                receivedMsgs.offer(msg, timeOut, TimeUnit.MILLISECONDS);
+                System.out.println(new String(msg));
             } catch (IOException ioe) {
                 ioe.printStackTrace();
             } catch (InterruptedException ie) {
-				ie.printStackTrace();
-			}
+                ie.printStackTrace();
+            }
         }
-	}
-	
-	public void stop(){
-		// interrupt a blocked socket.
-		isRunning = false;
-		mySocket.close();
-	}
-	
-	public BlockingQueue<byte[]> getMsgQueue()	{
-		return receivedMsgs;
-	}
-	
-    public static void main(String[] args)  {
-        Receiver receiver = new Receiver(8008);
+    }
+
+    public void stop() {
+        // interrupt a blocked socket.
+        isRunning = false;
+        mySocket.close();
+    }
+
+    public BlockingQueue<byte[]> getMsgQueue() {
+        return receivedMsgs;
+    }
+
+    public static void main(String[] args) {
+        String ipAddress = null;
+        int port = -1;
+        if (args.length < 1) {
+
+        }
+        // Check given file is Simple Text File using Java
+        else if (args.length == 1) {
+            try {
+                Scanner reader = new Scanner(new FileInputStream(args[0]));
+                ipAddress = reader.nextLine();
+                port = reader.nextInt();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else if (args.length == 2)  {
+            port = Integer.parseInt(args[1]);
+        }
+
+        Receiver receiver = new Receiver(port);
         new Thread(receiver).start();
     }
 }
