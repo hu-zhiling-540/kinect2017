7311054dc86c0793028203b933007b7af9214ea1
Zhiling <hu23z@mtholyoke.edu>
2017-06-13 17:56:08 -0400

    packet serialization and split up packet

---
+++ Kinect_UDP_Sender/Kinect_UDP_Sender/Packet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinect_UDP_Sender
+{
+    class Packet
+    {
+        public Packet(long timeStamp, int serialNum, int totalCount, byte[] data)
+        {
+            this.TimeStamp = timeStamp;
+            this.SerialNum = serialNum;
+            this.TotalCount = totalCount;
+            this.Data = data;
+        }
+        public string Name { get; set; }
+        public byte[] Data { get; set; }
+        public long TimeStamp { get; set; }
+        public int SerialNum { get; set; }
+        public int TotalCount { get; set; }
+        
+    }
+
+    static class Serialization
+    {
+        /// <summary>
+        /// Convert an object to a byte array
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <returns></returns>
+        public static byte[] Serialize(this Packet packet)
+        {
+            // construct a BinaryFormatter and use it to serialize the data to the stream
+            BinaryFormatter formatter = new BinaryFormatter();
+            // dispose memory stream once done processing it
+            using (var memstrm = new MemoryStream())
+            {
+                // serialize the packet into the stream
+                formatter.Serialize(memstrm, packet);
+                return memstrm.GetBuffer();
+            }
+        }
+    }
+}
--- Kinect_UDP_Sender/Kinect_UDP_Sender/Sender.cs
+++ Kinect_UDP_Sender/Kinect_UDP_Sender/Sender.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Net.Sockets;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Kinect_UDP_Sender
 {
@@ -30,29 +32,73 @@ namespace Kinect_UDP_Sender
         ///<summary>
         ///Sends messages of type Byte
         ///</summary>
-        public void SendMessage(byte[] msg)
-        {
-            var maxData = new byte[upperLimit];
-            int remaining = msg.Length;
-            while(remaining > 0)
-            {
-                int size = Math.Min(remaining, upperLimit);
-                Array.Copy(msg, msg.Length - remaining, maxData, 0, upperLimit);
-                mySocket.SendTo(maxData, remoteIPEP);
-                remaining -= size;
-            }
+        //public void SendMessage(byte[] msg)
+        //{
+        //    var maxData = new byte[upperLimit];
+        //    int remaining = msg.Length;
+        //    while(remaining > 0)
+        //    {
+        //        int size = Math.Min(remaining, upperLimit);
+        //        Array.Copy(msg, msg.Length - remaining, maxData, 0, upperLimit);
+        //        mySocket.SendTo(maxData, remoteIPEP);
+        //        remaining -= size;
+        //    }
 
-        }
+        //}
 
         ///<summary>
         ///Sends messages of type string
         ///</summary>
         public void SendMessage(string msg)
         {
-            SendMessage(Encoding.ASCII.GetBytes(msg));
-            //mySocket.SendTo(Encoding.ASCII.GetBytes(msg), remoteIPEP);
+            //SendMessage(Encoding.ASCII.GetBytes(msg));
+            mySocket.SendTo(Encoding.ASCII.GetBytes(msg), remoteIPEP);
         }
 
+
+        public void SendMessage(byte[] msg, long timeStamp)
+        {
+            // if the message exceeds over the maximum size
+            if (msg.Length > upperLimit)
+            {
+                // count of packets to be divided into
+                int count = msg.Length / upperLimit;
+                // leftover message for the last packet
+                int remainder = msg.Length % upperLimit;
+                // if has remainder, add up one more packet
+                if (remainder > 0)
+                    count += 1;
+                // check if more than once packet to be sent
+                if (count > 1)
+                {
+                    ICollection<Packet> packets = SplitUpMsg(timeStamp, count, remainder, msg);
+                    foreach (Packet packet in packets)
+                    {
+                        mySocket.SendTo(packet.Serialize(), remoteIPEP);
+                    }
+                }
+            }
+            else
+            {
+                mySocket.SendTo(msg, remoteIPEP);
+            }
+        }
+
+        public ICollection<Packet> SplitUpMsg(long timeStamp, int count, int remainder, byte[] msg)
+        {
+            List<Packet> packets = new List<Packet>();
+            for ( int i = 1; i < count; i++)
+            {
+                byte[] packetData = new byte[upperLimit];
+                Buffer.BlockCopy(msg, (i - 1) * upperLimit, packetData, 0, upperLimit);
+                packets.Add(new Packet(timeStamp, i, count, packetData));
+            }
+
+            //for the remainder packet
+
+
+            return packets;
+        }
 
     }
 }
