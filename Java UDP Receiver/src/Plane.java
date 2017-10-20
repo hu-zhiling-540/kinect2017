4d36bf950a01f6e7f852925c1249714ee2056276
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-10-19 23:59:47 -0400

    a new wrapper that takes all information about a plane

---
+++ Java UDP Receiver/src/Plane.java
@@ -0,0 +1,26 @@
+
+public class Plane {
+
+	double a;
+	double b;
+	double c;
+	double d;
+	
+//	n = a normal vector
+//	(x0, y0, z0) point on the plane
+			
+	/**
+	 * scalar equation of plane
+	 * a normal vector for the plane
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param d
+	 */
+	public plane(double a, double b, double c, double d)	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+}
